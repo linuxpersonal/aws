a48d075a4f4ea0d0074d1b49fed3dccd88a1e2e0
root <root@python.skull.local>
2017-11-26 12:30:49 +1100

    Update

--- manage_instances.py
+++ manage_instances.py
@@ -3,50 +3,67 @@
 import boto3
 import sys
 
-if len(sys.argv) != 3:
-    print("Usage: start/stop %s Instance_ID/Name" % sys.argv[0])
+if len(sys.argv) != 3 and len(sys.argv) !=2:
+    print("Usage: %s start/stop/list Instance_ID/Name(Not required for list)" 
+          % sys.argv[0])
     exit()
 
+ec2 = boto3.resource('ec2')
 client = boto3.client('ec2')
 response = client.describe_instances() 
 instance_id = False
 
-for item in response['Reservations']:
-    for list in item['Instances']:
-        name = str(list['Tags'][0]['Value'])
-	id = str(list['InstanceId'])	
-
-        if name == sys.argv[2]:
-            instance_id = id
-
-if not instance_id:
-    instance_id = sys.argv[2]
-
-if sys.argv[1] == 'start':
-
-    response = client.start_instances(
-        InstanceIds=[
-            instance_id,
-        ],
-        DryRun=False
-    )
-
-elif sys.argv[1] == 'stop':
+if sys.argv[1] == 'list':
     
-    response = client.stop_instances(
-        InstanceIds=[
-            instance_id,
-        ],
-        DryRun=False,
-        Force=True
-    )
+    for instance in ec2.instances.all():
+        for tag in instance.tags:
+            if tag['Key'] == 'Name':
+                print "\nInstance Name: " + tag['Value']
 
-elif sys.argv[1] == 'terminate':
-    response = client.terminate_instances(
-	InstanceIds=[
-	    instance_id,
-	],
-	DryRun=False
-    )
+        print "Instance ID: ", instance.id
+        print "Instance State: ", instance.state['Name']
+        print "Instance Code: ", instance.state['Code']
 
-print(response)
+    print("")
+
+else:
+    for item in response['Reservations']:
+        for list in item['Instances']:
+            name = str(list['Tags'][0]['Value'])
+            id = str(list['InstanceId'])	
+
+            if name == sys.argv[2]:
+                instance_id = id
+
+    if not instance_id:
+        instance_id = sys.argv[2]
+
+    if sys.argv[1] == 'start':
+
+        response = client.start_instances(
+            InstanceIds=[
+                instance_id,
+            ],
+            DryRun=False
+        )
+
+    elif sys.argv[1] == 'stop':
+        
+        response = client.stop_instances(
+            InstanceIds=[
+                instance_id,
+            ],
+            DryRun=False,
+            Force=True
+        )
+
+    elif sys.argv[1] == 'terminate':
+        response = client.terminate_instances(
+            InstanceIds=[
+                instance_id,
+            ],
+            DryRun=False
+        )
+
+
+    print(response)
